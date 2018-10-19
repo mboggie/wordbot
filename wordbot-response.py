beeb554f4659db13b7274b926fb6826173ef32c6
Adam Bloom <adamsbloom@gmail.com>
2018-10-19 15:30:11 -0400

    Fix bugs in response code. Add missing boto3 requirement to requirements file. Update sample config with appToken key.

--- wordbot-response.py
+++ wordbot-response.py
@@ -1,9 +1,10 @@
 import boto3
 import configparser
 import json
-import urllib
+import requests
 
 debug = False
+dryRun = False
 
 def lambda_handler(data, context):
 
@@ -13,12 +14,11 @@ def lambda_handler(data, context):
     if debug:
         print(config.sections())
 
-    APP_TOKEN = config['slack'].appToken
-    BOT_TOKEN = config['slack'].botToken
-    POST_URL = config['slack'].postUrl
+    APP_TOKEN = config['slack']['appToken']
+    POST_URL = config['slack']['postUrl']
 
-    S3_KEY = config['s3'].key
-    S3_BUCKET = config['s3'].bucket
+    S3_KEY = config['s3']['key']
+    S3_BUCKET = config['s3']['bucket']
 
     ### 1 - Get the slack event data
     eventToken = data['token']
@@ -34,6 +34,7 @@ def lambda_handler(data, context):
 
     ### 4 - If the message is a "challenge", respond accordingly
     elif 'challenge' in data:
+        print ('*** challenge accepted:', data['challenge'])
         return data['challenge']
 
     ### 5 - If the message contains the secret word,
@@ -42,33 +43,30 @@ def lambda_handler(data, context):
         ### 5a - Retrieve the secret word from S3
         client = boto3.client('s3')
         s3response = client.get_object(Bucket=S3_BUCKET, Key=S3_KEY)
-        word = json.loads(s3response.Body.read())['word']
+        word = json.loads(s3response['Body'].read())['word']
 
         ### 5b - If the word is contained within the message,
         ###      construct and post response
-        if word in event['text']:
-            channel = event['channel']
+        if word.lower() in event['text'].lower():
             responseText = 'AAAAAAAAAA!!!!!'
-            data = urllib.parse.urlencode(
-                (
-                    ("token", BOT_TOKEN),
-                    ("channel", channel),
-                    ("text", responseText)
-                )
-            )
-            data = data.encode("ascii")
+            response = {'text': responseText, 'mrkdwn': True}
+            if not dryRun:
+                requests.post(POST_URL, json.dumps(response))
+            print (response)
 
-            request = urllib.request.Request(
-                POST_URL,
-                data=data,
-                method="POST"
-            )
-            request.add_header(
-                "Content-Type",
-                "application/x-www-form-urlencoded"
-            )
-
-            urllib.request.urlopen(request).read()
+        else:
+            print ("*** word not in message... ignoring")
 
     ### end
     return "200 OK"
+
+if __name__ == "__main__":
+    debug = True
+
+    testData = {
+        'token': '0c51a8a8afc3d190f55fd848200d5842',
+        'event': {
+            'text': 'this is a Paleotechnical test'
+            }
+        }
+    lambda_handler(testData, None)
